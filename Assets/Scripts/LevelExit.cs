9337da95e0b2ac40c8a93e21ffd9484aebd72a04
Jared Farkas <jaredmfarkas@gmail.com>
2023-04-10 09:19:29 -0700

    Add level exit portal

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 1f;
+    void OnTriggerEnter2D(Collider2D other) {
+
+        StartCoroutine(LoadNextLevel());
+         
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
